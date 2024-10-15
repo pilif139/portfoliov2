5859b9c58b49810215d733bc2889ea30abd0efff
pilif139 <pifilif@gmail.com>
2024-10-15 22:37:18 +0200

    added animation and html button tag

--- src/app/AccountDropdownMenu.tsx
+++ src/app/AccountDropdownMenu.tsx
@@ -3,6 +3,7 @@
 import Link from "next/link";
 import { MdOutlineAccountCircle } from "react-icons/md";
 import { useEffect, useRef, useState } from "react";
+import { AnimatePresence, motion } from "framer-motion";
 
 export default function AcccountDropdownMenu(){
     const [isOpen, setIsOpen] = useState(false);
@@ -31,20 +32,30 @@ export default function AcccountDropdownMenu(){
 
     return (
         <div ref={node}>
-            <MdOutlineAccountCircle size={35} className="hover:text-nord-10 transition duration-300 cursor-pointer" onClick={()=>setIsOpen(!isOpen)}/>
+            <button 
+                className="hover:text-nord-10 transition duration-300 cursor-pointer flex items-center" 
+                onClick={()=>setIsOpen(!isOpen)}>
+                <MdOutlineAccountCircle size={35}/>
+            </button>
+            <AnimatePresence>
             { isOpen && (
-                <div className='absolute top-16 bg-white text-black right-0 mt-2 py-2 w-48 border rounded shadow-xl'>
-                <Link href='/profile' className='block px-4 py-2 hover:bg-gray-200'>
-                    Profile
-                </Link>
-                <Link href='/settings' className='block px-4 py-2 hover:bg-gray-200'>
-                    Settings
-                </Link>
-                <Link href='/logout'  className='block px-4 py-2 hover:bg-gray-200'>
-                    Logout
-                </Link>
-            </div>
+                <motion.div 
+                    initial={{ opacity: 0, y: -10 }}
+                    animate={{ opacity: 1, y: 0 }}
+                    exit={{ opacity: 0, y: -10 }}
+                    className='absolute top-16 bg-nord-3 text-nord-5 right-0 mt-2 py-2 w-48 rounded shadow-xl shadow-gray-950'>
+                    <Link href='/profile' className='block px-4 py-2 hover:bg-nord-2'>
+                        Profile
+                    </Link>
+                    <Link href='/settings' className='block px-4 py-2 hover:bg-nord-2'>
+                        Settings
+                    </Link>
+                    <Link href='/logout'  className='block px-4 py-2 hover:bg-nord-2'>
+                        Logout
+                    </Link>
+                </motion.div>
             )}
+            </AnimatePresence>
         </div>
     )
 }
