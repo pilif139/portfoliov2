15ff50a5c228a9cb408672006d6f6764a1d844fd
pilif139 <pifilif@gmail.com>
2024-10-31 22:08:54 +0100

    primary-2 variant button

--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -5,20 +5,21 @@ type ButtonProps = {
   type?: "button" | "submit" | "reset";
   onClick?: (e : MouseEvent<HTMLButtonElement>) => void;
   className?: string;
-  variant?: "primary" | "secondary";
+  variant?: "primary" | "primary-2" | "secondary" | "error";
 };
 
 export default function Button({ children, type="button", onClick, className, variant} : ButtonProps) {
   const variants = {
     "primary": "bg-nord-10 hover:bg-nord-9",
+    "primary-2": "bg-nord-14 hover:bg-nord-13",
     "secondary": "bg-nord-0 hover:bg-nord-1 border-2 border-white",
     "error": "bg-nord-11 hover:bg-nord-12"
   }
   const variantStyle = variants[variant || "primary"];
 
   return (
-    <button type={type} className={`text-white font-bold py-2 px-4 rounded-lg transition duration-300 w-fit 
-    ${variantStyle} ${className}`} onClick={onClick}>
+    <button type={type} className={`${variantStyle} ${className} text-white font-bold py-2 px-4 rounded-lg transition duration-300 w-fit 
+     `} onClick={onClick}>
         {children}
     </button>
   );
