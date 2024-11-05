ffa2b7ef3d6d8c3e670fc3cff29001ce671f6ea8
pilif139 <pifilif@gmail.com>
2024-11-05 20:23:54 +0100

    changed file uplod from client side to server side

--- src/app/admin/posts/new/page.tsx
+++ src/app/admin/posts/new/page.tsx
@@ -1,62 +1,62 @@
 "use client";
 
 import Heading from "@/components/ui/Heading";
-import { upload } from "@vercel/blob/client";
 import ContentsList from "./ContentsList";
 import FadeDiv from "@/components/ui/FadeDiv";
 import CreatePostForm from "@/components/CreatePostForm";
 import { useCreatePostContext } from "@/components/CreatePostContextProvider";
 import ContentForm from "@/components/ContentForm";
-import base64ToFile from "@/lib/utils/base64ToFile";
 import CreatePost from "@/server/post/createPost";
-import submitFileOnLocalhost from "@/server/post/submitFilesToDatabaseLocal";
+import submitFileToVercelStorage, { FileContent } from "@/server/post/submitFileToVercelStorage";
+import base64ToFile from "@/lib/utils/base64ToFile";
 
-export default function NewPostPage(){
-    const { contents, title, description,  } = useCreatePostContext();
-    
+export default function NewPostPage() {
+    const { contents, title, description, } = useCreatePostContext();
+
     const handleCreatePost = async (e: React.MouseEvent<HTMLButtonElement>) => {
         e.preventDefault();
-        const {post_id, errors} = await CreatePost({title, description, contents});
-        if(!post_id){
+        const { post_id, errors } = await CreatePost({ title, description, contents });
+        if (!post_id) {
             return;
         }
-        if(errors){
+        if (errors) {
             throw new Error("An error occurred while creating the post");
         }
-        if(process.env.NODE_ENV === "production"){
-            await submitFileToVercelStorage(post_id);
-        } else {
-            const files = contents.filter(content => content.type === "file" || content.type === "image" || content.type === "video");
-            await submitFileOnLocalhost(post_id, files);
+        const files = contents
+                        .filter((content) => content.type === "file" || content.type === "image" || content.type === "video")
+                        .map((file) => {
+                            if (file.content && file.type && file.position && post_id && post_id) {
+                                return {
+                                    post_id,
+                                    position: file.position,
+                                    type: file.type,
+                                    content: base64ToFile(file.content, `${file.type}-${post_id}-${file.position}`)
+                                }
+                            }
+                            return null
+                        }).filter(file => file !== null) as FileContent[];
+        for(const file of files){
+            const formData = new FormData();
+            formData.append("file", file.content);
+            formData.append("type", file.type);
+            formData.append("position", file.position.toString());
+            formData.append("post_id", post_id.toString());
+            await submitFileToVercelStorage(formData);
         }
-    }
-
-    const submitFileToVercelStorage = async (post_id: number) =>{
-        const files = contents.filter(content => content.type === "file" || content.type === "image" || content.type === "video");
-        files.forEach(async (file) => {
-            const newFile = base64ToFile(file.content as string, `${post_id}-${file.position}`);
-            if(!(newFile instanceof File)){
-                throw new Error("An error occurred while creating the file");
-            }
-            await upload(newFile.name, newFile, {
-                access: "public",
-                handleUploadUrl: `/api/images?post_id=${post_id}&position=${file.position}`
-            })
-        });
     }
 
     return (
-            <FadeDiv className="w-full min-h-[75vh] flex flex-col items-center mx-0">
-                <Heading variant="2" className="text-nord-9">Create new post</Heading>
-                <div className="flex gap-4 w-full py-4 justify-between">
-                    <CreatePostForm handleCreatePost={handleCreatePost}/>
-                    <article className="w-[50%] flex flex-col px-8">
-                        <ContentsList/>
-                    </article>
-                    <aside className="w-max h-fit bg-nord-0 rounded-xl p-4">
-                        <ContentForm/>
-                    </aside>
-                </div>
-            </FadeDiv>
+        <FadeDiv className="w-full min-h-[75vh] flex flex-col items-center mx-0">
+            <Heading variant="2" className="text-nord-9">Create new post</Heading>
+            <div className="flex gap-4 w-full py-4 justify-between">
+                <CreatePostForm handleCreatePost={handleCreatePost} />
+                <article className="w-[50%] flex flex-col px-8">
+                    <ContentsList />
+                </article>
+                <aside className="w-max h-fit bg-nord-0 rounded-xl p-4">
+                    <ContentForm />
+                </aside>
+            </div>
+        </FadeDiv>
     )
 }
--- src/lib/utils/base64ToFile.ts
+++ src/lib/utils/base64ToFile.ts
@@ -22,29 +22,3 @@ export default function base64ToFile(base64String: string, fileName: string) : F
     // Create a File object from the Blob
     return new File([blob], fileName, { type: mimeType });
 }
-
-export function base64ToBlob(base64String: string, fileName: string){
-    // Extract the MIME type from the base64 string
-    const mimeType = base64String.match(/data:([^;]+);base64,/)![1];
-    const extension = mimeType.split('/')[1];
-
-    if (!fileName.endsWith(`.${extension}`)) {
-        fileName += `.${extension}`;
-    }
-    
-    // Decode the base64 string
-    const byteString = atob(base64String.split(',')[1]);
-    const ab = new ArrayBuffer(byteString.length);
-    const ia = new Uint8Array(ab);
-
-    for (let i = 0; i < byteString.length; i++) {
-        ia[i] = byteString.charCodeAt(i);
-    }
-
-    // Convert the decoded data into a Blob
-    const blob = new Blob([ab], { type: mimeType });
-
-    // Create a File object from the Blob
-    return new Blob([blob], { type: mimeType });
-}
-
