7ac162c6b80e1b510dba8587647dc597eaf39ca1
pilif139 <pifilif@gmail.com>
2024-11-08 21:00:48 +0100

    deleting posts

--- src/app/admin/posts/Post.tsx
+++ src/app/admin/posts/Post.tsx
@@ -1,62 +1,90 @@
 "use client";
 
 import Heading from "@/components/ui/Heading";
-import { Post } from "@/db/schema/posts"
+import { Post } from "@/db/schema/posts";
 import Link from "next/link";
 import { Spline_Sans_Mono as Font } from "next/font/google";
 import { MdDelete, MdModeEdit } from "react-icons/md";
 import Button from "@/components/ui/Button";
 import { useModalContext } from "@/components/ModalContext";
+import deletePost from "@/server/post/deletePost";
 
 const font = Font({
-    subsets: ["latin"],
-    weight: "600"
+  subsets: ["latin"],
+  weight: "600",
 });
 
 type PostProps = {
-    post: Post;
-}
+  post: Post;
+};
 
 export default function PostCard({ post }: PostProps) {
-    const { openModal } = useModalContext();
+  const { openModal, closeModal } = useModalContext();
 
-    const handleDelete = () => {
-        openModal(<DeleteModal />);
-    }
+  const handleDelete = () => {
+    openModal(<DeleteModal onClick={()=>{
+        deletePost(post.id);
+        closeModal();
+    }}/>);
+  };
 
-    return (
-        <div className="bg-nord-0 hover:bg-gray-800 transition p-4 rounded-xl cursor-pointer w-[20vw] h-full">
-            <Link href={`/admin/posts/${post.id}`}>
-                <div>
-                    <Heading variant="3" className="text-nord-9">{post.title}</Heading>
-                    <p className={`text-nord-4 ${font.className}`}>{post.description}</p>
-                </div>
-            </Link>
-            <div className="mt-4 flex gap-4">
-                <Link href={`/admin/posts/edit/${post.id}`}>
-                    <Button variant="secondary"
-                        className="flex items-center gap-2 text-xl"
-                    >
-                        Edit <MdModeEdit size={20} />
-                    </Button>
-                </Link>
-                <Button variant="secondary"
-                    className="flex items-center gap-2 text-xl bg-nord-11 hover:bg-red-400"
-                    onClick={handleDelete}
-                    >
-                    Delete <MdDelete size={20} />
-                </Button>
-            </div>
-        </div>
-    )
-}
-
-const DeleteModal = () => {
-    return (
+  return (
+    <div className="bg-nord-0 hover:bg-gray-800 transition p-4 rounded-xl cursor-pointer w-[20vw] h-full">
+      <Link href={`/admin/posts/${post.id}`}>
         <div>
-            <Heading variant="2">Are you sure you want to delete this post?</Heading>
-            <Button variant="secondary">Yes</Button>
-            <Button variant="secondary">No</Button>
+          <Heading variant="3" className="text-nord-9">
+            {post.title}
+          </Heading>
+          <p className={`text-nord-4 ${font.className}`}>{post.description}</p>
         </div>
-    )
+      </Link>
+      <div className="mt-4 flex gap-4">
+        <Link href={`/admin/posts/edit/${post.id}`}>
+          <Button
+            variant="secondary"
+            className="flex items-center gap-2 text-xl"
+          >
+            Edit <MdModeEdit size={20} />
+          </Button>
+        </Link>
+        <Button
+          variant="secondary"
+          className="flex items-center gap-2 text-xl bg-nord-11 hover:bg-red-400"
+          onClick={handleDelete}
+        >
+          Delete <MdDelete size={20} />
+        </Button>
+      </div>
+    </div>
+  );
 }
+
+const DeleteModal = ({onClick} : {onClick: ()=>void}) => {
+  const { closeModal } = useModalContext();
+
+  return (
+    <div className="flex gap-2 flex-col w-[30vw]">
+      <Heading variant="2">Are you sure you want to delete this post?</Heading>
+      <p>
+        This will permamently delete all the data, images and all information
+        about this post.
+      </p>
+      <div className="flex gap-4">
+        <Button
+          variant="secondary"
+          className="bg-nord-14 hover:bg-green-500 text-xl"
+          onClick={onClick}
+        >
+          Yes
+        </Button>
+        <Button
+          variant="secondary"
+          className="bg-nord-11 hover:bg-red-400 text-xl"
+          onClick={() => closeModal()}
+        >
+          No
+        </Button>
+      </div>
+    </div>
+  );
+};
---
+++ src/server/post/deletePost.ts
@@ -0,0 +1,35 @@
+"use server"
+
+import db from "@/db/db"
+import { post_content_blocksTable, postsTable } from "@/db/schema/posts"
+import { del } from "@vercel/blob";
+import { eq, or } from "drizzle-orm"
+import { revalidatePath } from "next/cache";
+
+export default async function deletePost(postId : number){
+    await db.transaction(async (tx)=>{
+        const [post] = await tx.select().from(postsTable).where(eq(postsTable.id, postId)).execute();
+        if(!post){
+            return {error: "Post not found"};
+        }
+        const filesBlocks = await tx.select()
+                                    .from(post_content_blocksTable)
+                                    .where(or(
+                                        eq(post_content_blocksTable.type, "image"),
+                                        eq(post_content_blocksTable.type, "file"),
+                                        eq(post_content_blocksTable.type, "video")
+                                    ))
+                                    .execute();
+        for(const block of filesBlocks){
+            try{
+                await del(block.content);
+            } catch(e){
+                console.error(e);
+                return {error: "Error deleting files from vercel blob storage "+e};
+            }
+        }
+        await tx.delete(post_content_blocksTable).where(eq(post_content_blocksTable.post_id, postId)).execute();
+        await tx.delete(postsTable).where(eq(postsTable.id, postId)).execute();
+    })
+    revalidatePath("/admin/posts");
+}
